f2162bcec24d953cf82539e9246b9954c3b0bf6a
Jeremy Hastings <jeremy.hastings@mac.com>
2020-03-21 10:03:25 +0000

    Added validation to Article Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
-  
+  # Enforces that the title is not nil, a minimum of 3 characters and a
+  # maximum of 50.
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  # Enforces that the description is not nil, a minimum of 10 characters and a
+  # maximum of 300.
+  validates :description, presence: true, length: { minimum: 10, maximum: 300}
 end
