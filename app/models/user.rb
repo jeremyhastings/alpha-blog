4cba4b90050b507ab52cd1b7a5018c65dc607ce8
Jeremy Hastings <jeremy.hastings@mac.com>
2020-03-30 13:55:30 -0700

    Added length validation to :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates :user, presence: true, uniqueness: true
+  validates :user, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
