cec2d2205ab3220375af612f12486ce7147e61ae
Jeremy Hastings <jeremy.hastings@mac.com>
2020-03-30 15:00:38 -0700

    Makes sure that there is a foreign key present

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  # Enforces that the title is not nil, a minimum of 3 characters and a
-  # maximum of 50.
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  # Enforces that the description is not nil, a minimum of 10 characters and a
-  # maximum of 300.
+
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :user_id, presence: true
 end
