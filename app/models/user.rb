f9497a253b9173997b826d9fb4e20842ae524324
Jeremy Hastings <jeremy.hastings@mac.com>
2020-03-30 14:00:19 -0700

    Corrected :username validation to actually be :username not :user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates :user, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: true
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
