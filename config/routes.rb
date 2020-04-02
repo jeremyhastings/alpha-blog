dfeed11a7e66b9832d5e0544be2d58502208193a
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-02 09:26:06 -0700

    get 'signup', to: 'users#new'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   # a get request with /about that directs to pages controller about action
   get 'about', to: 'pages#about'
+
+  get'signup', to: 'users#new'
   # Creates new_article (post to create), edit (patch to update), delete
   # article, show, and index article paths
   resources :articles
