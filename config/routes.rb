dfbb05ddd786dafa59b6110c4f40635dbb408b29
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-02 10:36:43 -0700

    Updated routes to create users resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   # a get request with /about that directs to pages controller about action
   get 'about', to: 'pages#about'
-
-  get'signup', to: 'users#new'
   # Creates new_article (post to create), edit (patch to update), delete
   # article, show, and index article paths
   resources :articles
+  # user paths with new users being created at 'signup'
+  resources :users, except:[:new]
+  get 'signup', to: 'users#new'
 end
