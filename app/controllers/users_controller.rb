881aff73c13c798615b3761d895d6ef2d4f484b3
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-03 09:19:56 -0700

    Created user_articles instance variable in user_controller.  Paginated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.order(:created_at).page params[:page]
   end
 
   private
