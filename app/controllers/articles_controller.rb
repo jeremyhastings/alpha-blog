5a2e939d81204fad1062f41f2e94552e8d50640e
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-03 14:43:48 -0700

    before_action :require_same_user, only: [:edit, :update, :destroy]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.order(:created_at).page params[:page]
