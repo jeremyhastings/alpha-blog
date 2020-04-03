0492f77918c96548116e3e2a09bafefc940438a3
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-03 11:33:31 -0700

    Updated navigation partial to handle logged in user or no user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,14 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-
+    !!current_user
   end
 
   def require_user
-
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
   end
 
 end
