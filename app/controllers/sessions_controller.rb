87b94c16d162aaa7ddda0149e6a67d0cc7f55af1
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-03 11:03:02 -0700

    Created logout button in navigation.  Updated session controller to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to users_path(user)
+      redirect_to user_path(user)
     else
       # No validation because there is no model
       flash.now[:danger] = "There was something wrong with your login information"
