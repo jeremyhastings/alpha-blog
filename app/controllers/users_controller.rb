53cae292f0fe7e155ee81f4144517f579000fd9a
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-04 08:47:00 -0700

    Added require_admin method to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.order(:username).page params[:page]
@@ -58,6 +59,13 @@ class UsersController < ApplicationController
   def require_same_user
     if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account."
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
