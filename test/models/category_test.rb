4175b78fc8494015134fcea46eac1139a18089c8
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-04 11:52:56 -0700

    Created setup method in CategoryTest Class

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,5 +2,12 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  
+  def setup
+    @category = Category.new(name: "sports")
+  end
+
+  test "category should be valid" do
+    
+  end
+
 end
