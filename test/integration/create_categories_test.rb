f3f629bfbbb55059d825c414b66c3331d413f7db
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-06 12:23:29 -0700

    Updated Gemfile and added test for creating a category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,5 +2,10 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  test "get new category form and create category" do
+    get new_category_path
+    assert_template 'categories/new'
+  end
+
 
 end
