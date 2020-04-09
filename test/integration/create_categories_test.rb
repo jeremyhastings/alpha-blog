5d6e64a7931db8edaf21422168a5645fcd7a4c39
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-09 13:57:01 -0700

    Created setup method with admin user

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: "jeremy", email: "jeremy@example.com", password: "password", admin: true)
+  end
+
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
