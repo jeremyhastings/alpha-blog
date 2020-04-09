b16280cfe8825d85eced3932b782b04f3450c212
Jeremy Hastings <jeremy.hastings@mac.com>
2020-04-09 16:29:02 -0700

    Associate categories and articles with one another.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,8 @@ class Category < ApplicationRecord
 
   paginates_per 5
 
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
 end
